ec1d3fd3b04a5b5e897549d8720f3c14bc9f7035
TapZe <nabilmuyassar@Gmail.com>
2024-11-07 11:16:25 +0800

    feat: store.ts, transaction and theme reducers

---
+++ src/redux/store.ts
@@ -0,0 +1,55 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+import { combineReducers } from "redux";
+import { setupListeners } from "@reduxjs/toolkit/query";
+
+// Import reducer here
+import themeReducer from "./reducers/themeSlice";
+import { transactionQueryAPI } from "./reducers/transactions/transactionQueryAPI";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+// If the reducers that need persist is more than one
+const rootReducer = combineReducers({
+  theme: themeReducer,
+});
+
+// for only one reducer needs persist, rootReducer can be replaced with the one reducer that needs it.
+// it is recommended for future coding that it's saved in a root reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: {
+    persist: persistedReducer,
+    [transactionQueryAPI.reducerPath]: transactionQueryAPI.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }).concat(transactionQueryAPI.middleware),
+});
+
+// Setup persistor
+export const persistor = persistStore(store);
+
+// Type Export
+export type RootState = ReturnType<typeof store.getState>;
+
+// optional, but required for refetchOnFocus/refetchOnReconnect behaviors
+// see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
+setupListeners(store.dispatch)
